57716d241e620447d52f350ffed05fb553eab3ba
Spencer Alves <impiaaa@gmail.com>
2014-08-23 12:08:55 -0700

    Scene scaling test

---
+++ LD30/Assets/Scripts/ScaleScene.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+private var hasDisabled : boolean;
+private var rigidbodies : Rigidbody[];
+private var behaviors : Behaviour[];
+
+function Start () {
+    rigidbodies = GetComponentsInChildren.<Rigidbody>();
+    behaviors = GetComponentsInChildren.<Behaviour>();
+}
+
+private function enableEverything(enable : boolean) {
+    for (var body : Rigidbody in rigidbodies) {
+        body.isKinematic = !enable;
+    }
+    for (var component : Behaviour in behaviors) {
+        if (component !== this) {
+            component.enabled = enable;
+        }
+    }
+}
+
+function Update () {
+    var flatAmt = Input.GetAxis("Fire3");
+    var isFlattening = flatAmt > 0;
+    var isFlattened = flatAmt >= 1;
+    transform.localScale.y = 1-(flatAmt*0.9);
+    if (isFlattening && !hasDisabled) {
+        enableEverything(false);
+        hasDisabled = true;
+    }
+    else if (!isFlattening && hasDisabled) {
+        enableEverything(true);
+        hasDisabled = false;
+    }
+}
