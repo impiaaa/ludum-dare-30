0a4a24ecd10c4dcecdf9bdcdb2c1fade9a0b899e
Spencer Alves <impiaaa@gmail.com>
2014-08-24 00:15:17 -0700

    Text stuff & updated scene

--- LD30/Assets/Scripts/TextController.js
+++ LD30/Assets/Scripts/TextController.js
@@ -1,9 +1,18 @@
 ﻿#pragma strict
 
-function Start () {
+var textBits:String[];
+var bitsShown:int;
 
+private var textMesh:TextMesh;
+
+function Start () {
+    textMesh = GetComponent.<TextMesh>();
 }
 
 function Update () {
-
+    var str:String = "";
+    for (var i:int = 0; i < bitsShown; i++) {
+        str += textBits[i];
+    }
+    textMesh.text = str;
 }
